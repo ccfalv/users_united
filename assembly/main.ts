ec93e9e464036f3dac9bb3ee481793001439c525
swain <iamswain@hotmail.com>
2019-09-09 15:19:52 -0700

    error on assembly

---
+++ assembly/main.ts
@@ -0,0 +1,36 @@
+import { storage, PersistentVector } from "near-runtime-ts";
+import { PostedMessage } from "./model";
+let messages = new PersistentVector<PostedMessage>("m");
+export function addMessage(
+  text: string,
+  id: string,
+  name: string,
+  photo: string,
+  email: string,
+  date: string
+): void {
+  let message: PostedMessage = {
+    id,
+    text,
+    name,
+    photo,
+    date,
+    email,
+    index: 0
+  };
+  storage.setString(id, date);
+  messages.push(message);
+}
+
+export function getRangeMessages(start: i32 = 0): Array<PostedMessage> {
+  let numMessages: i32 = messages.length < start ? 0 : messages.length - start;
+  let result = Array.create<PostedMessage>(numMessages);
+  if (numMessages > 0) {
+    let startIndex = start;
+    for (let i = 0; i < numMessages; i++) {
+      result[i] = messages[i + startIndex];
+      result[i].index = i + startIndex;
+    }
+  }
+  return result;
+}
---
+++ assembly/model.ts
@@ -0,0 +1,10 @@
+// Exporting a new class PostedMessage so it can be used outside of this file.
+export class PostedMessage {
+  id: string;
+  name: string;
+  text: string;
+  index: u64;
+  date: string;
+  photo: string;
+  email: string;
+}
