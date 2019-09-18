3c4e7c7f989669899e83b1d3ae2aee90bf4b5500
swain <iamswain@hotmail.com>
2019-09-17 18:51:17 -0700

    contract counter

--- assembly/main.ts
+++ assembly/main.ts
@@ -20,13 +20,13 @@ export function addMessage(
     email
   };
   const vMsg: VectorMessage = { id, text, name, photo, date };
-  // if (!storage.get<PostedMessage>(id)) {
-  const yearMonth = date.substr(0, 7);
-  const counter: i32 = storage.getPrimitive<i32>(yearMonth, 0);
-  storage.set<i32>(yearMonth, counter + 1);
-  storage.set<PostedMessage>(id, pMsg);
-  messages.push(vMsg);
-  // }
+  if (!storage.contains(id)) {
+    const yearMonth = date.substring(0, 7);
+    const counter = storage.getPrimitive<i32>(yearMonth, 0);
+    storage.set<i32>(yearMonth, counter + 1);
+    storage.set<PostedMessage>(id, pMsg);
+    messages.push(vMsg);
+  }
 }
 
 export function getRangeMessages(start: i32 = 0): Array<VectorMessage> {
@@ -44,8 +44,21 @@ export function hasCommented(id: string): PostedMessage | null {
   return storage.get<PostedMessage>(id);
 }
 
-export function monthCounter(yearMonth: string): i32 {
+export function getMonthCounter(yearMonth: string): i32 {
   return storage.getPrimitive<i32>(yearMonth, 0);
+}
+export function getContainsId(id: string = "test"): bool {
+  return storage.contains(id);
+}
+
+export function setMonthCounter(date: string): bool {
+  const yearMonth = date.substring(0, 7);
+  if (storage.contains(yearMonth)) {
+    const counter = storage.getPrimitive<i32>(yearMonth, 0);
+    storage.set<i32>(yearMonth, counter + 1);
+    return true;
+  }
+  return false;
 }
 
 export function monthCounters(yearMonth6: string): Array<i32> {
